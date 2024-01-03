1e637398b866e79c556a40b193ecbddbc0df2ec9
SamH434 <77312898+SamH434@users.noreply.github.com>
2024-01-03 02:49:09 -0500

    final version completed

--- Game.java
+++ Game.java
@@ -5,36 +5,44 @@ class Game {
     private Deck deck;
     private int bet;
 
+
+
+
+
     public Game(Person player, Deck deck, int bet) {
         this.player = player;
         this.deck = deck;
         this.bet = bet;
     }
 
-    public void play() {
-        System.out.println("Starting a new game...");
 
-        // Deal initial cards
+
+
+
+
+    public void play() {
+        //starting sequence
+        System.out.println("Starting a new game...");
         Card playerCard1 = deck.drawCard();
         Card dealerCard1 = deck.drawCard();
         Card playerCard2 = deck.drawCard();
         Card dealerCard2 = deck.drawCard();
-
-        // Display initial cards
         System.out.println("Your cards: " + playerCard1 + ", " + playerCard2);
         System.out.println("Dealer's upcard: " + dealerCard1);
 
-        // Player's turn
+        
         int playerSum = playerCard1.getValue() + playerCard2.getValue();
         playerSum = playerTurn(playerSum);
-
-        // Dealer's turn
         int dealerSum = dealerCard1.getValue() + dealerCard2.getValue();
         dealerSum = dealerTurn(dealerSum);
 
-        // Determine winner
         determineWinner(playerSum, dealerSum);
     }
+
+
+
+//should we be setting a minimum amount? we have a placeholder
+
 
     private int playerTurn(int playerSum) {
         Scanner scanner = new Scanner(System.in);
@@ -47,7 +55,6 @@ class Game {
                 Card card = deck.drawCard();
                 System.out.println("You drew: " + card);
                 playerSum += card.getValue();
-
                 if (playerSum > 21) {
                     System.out.println("Bust! Your sum is over 21.");
                     return 0; // Player busts
@@ -62,10 +69,18 @@ class Game {
         return playerSum;
     } 
 
+
+
+
+
+
+
+
+
     private int dealerTurn(int dealerSum) {
         System.out.println("Dealer's turn...");
 
-        while (dealerSum < 17) {
+        while (dealerSum < 17) {//what
             Card card = deck.drawCard();
             System.out.println("Dealer drew: " + card);
             dealerSum += card.getValue();
@@ -75,6 +90,12 @@ class Game {
         return dealerSum;
     }
 
+
+
+
+
+
+//TODO:make sure this actually compares both 
     private void determineWinner(int playerSum, int dealerSum) {
         if (playerSum == 0) {
             player.updateMoney(-bet);
